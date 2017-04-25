14f3bcab1d9163a1e96b607942c27ead17f3a4de
nikhil <nikhil.deshpande@atmecs.com>
2017-04-25 18:13:20 +0530

    Changes are modified as per java 1.8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,7 +1,16 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     
+	List<String> list = new ArrayList<>();
+	public void print()
+	{
+		list.add("its in List..")
+	}
+	
     public Application() {
         System.out.println ("Inside Application");
     }
@@ -10,5 +19,7 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	app.print();
+
     }
 }
